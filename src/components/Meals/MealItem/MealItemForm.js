f28a5d95889f4b5fc557f9cc46ddf0ae02ca27ee
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-10-04 10:22:58 -0500

    REAFACTOR: Fixing the inputs ids

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -3,14 +3,17 @@ import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
     return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: 'amount',
-            type: 'numebr',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }}/>
+        <Input 
+            label="Amount" 
+            input={{
+                id: 'amount_' + props.id,
+                type: 'numebr',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }}
+        />
         <button>+ Add</button>
     </form>
 };
